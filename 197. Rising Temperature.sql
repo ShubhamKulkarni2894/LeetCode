4d4a03057316ba74fbcc26688631d616b769c7ec
unknown <kshubham2894@gmail.com>
2022-04-30 02:59:25 -0400

    30 Apr SQL Problem 197

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,50 @@
+/*
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+*/
+
+--Solution:
+
+
+Select w1.id from Weather w1 
+Inner join Weather w2
+on w1.temperature > W2.temperature
+AND DATEDIFF(day,w2.recorddate,w1.recorddate) =1
+
+
